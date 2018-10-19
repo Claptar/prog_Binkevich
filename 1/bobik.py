592697708bf36982e1ec1ce46f677574c821cc30
Claptar <aleksandr.binkevich@gmail.com>
2018-10-19 14:55:03 +0300

    Добавил счётчик кликов

--- 1/bobik.py
+++ 1/bobik.py
@@ -3,24 +3,40 @@ import graphics as gr
 import random
 import time
 
+root = Tk()
+root.geometry('800x600')
+n = 0
+R = 10
 
-def bobik():
-    x = random.randint(20, 580)
-    y = random.randint(20, 580)
-    canv.create_oval(x-10, y-10,x+10, y+10,fill=gr.color_rgb(random.randint(100, 255), random.randint(100, 255), random.randint(100 , 255)))
+
+def baloon():
+    global circle_x, circle_y, R
+    circle_x = random.randint(20, 580)
+    circle_y = random.randint(20, 580)
+    red=random.randint(100, 255)
+    green=random.randint(100, 255)
+    blue=random.randint(100, 255)
+    canv.create_oval(circle_x-R, circle_y-R, circle_x+R, circle_y+R, fill=gr.color_rgb(red, green, blue))
+
+
+def left_click(event):
+    x = event.x
+    y = event.y
+    global n
+    if abs(circle_x - x) < R+1 and abs(circle_y-y) < R+1:
+        n += 1
+
+
 
 
 def tick():
     root.after(1000, tick)
     canv.delete(ALL)
-    bobik()
+    baloon()
+    canv.create_text(400, 300, text=n, font='Arial 25')
 
-root.bind('<Button-1>',left_button)
 
-def left_button(event):
-    root.title("Left")
-root = Tk()
-root.geometry('800x600')
+root.bind('<Button-1>', left_click)
 
 canv = Canvas(root, bg='white')
 canv.pack(fill=BOTH, expand=1)
