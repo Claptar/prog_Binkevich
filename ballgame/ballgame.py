0f35b0209ba592543544bbe09fb36dd266e01cd1
Claptar <aleksandr.binkevich@gmail.com>
2018-11-01 23:12:18 +0300

    Сделал 20 летающих шариков, которые не взаимодействуют друг с другом. Поменял названия переменных. Каждый раз остаётся один неподвижный шарик, не знаю откуда он берётся. Ещё, когда я ставлю границу, где шарики должны менять своё направление, меньше размера окна, они начинают зацикливаться. PyCharm ругается, что переменная больше не используется, хотя она используется чуть ли не в следующей строке.

--- ballgame/ballgame.py
+++ ballgame/ballgame.py
@@ -1,27 +1,55 @@
 from tkinter import *
 import random
+import graphics as gr
 
 root = Tk()
-root.geometry("800x800")
+root.geometry("600x600")
 canvas = Canvas(root)
 r = 40
 canvas.pack(fill=BOTH, expand=1)
 ball = []
 
-for i in range(5):
+for i in range(20):
+    red = random.randint(100, 255)
+    green = random.randint(100, 255)
+    blue = random.randint(100, 255)
     x = random.randint(100, 600)
     y = random.randint(100, 600)
-    Vx = random.randint(1, 3)*i
-    Vy = random.randint(1, 3)*i
-    ball.append(canvas.create_oval(x, y, x+r, y+r, fill = "red"))
-    ball.append(Vx)
-    ball.append(Vy)
+    vx = random.randint(-2, 2)*i
+    vy = random.randint(-2, 2)*i
+    ball.append(canvas.create_oval(x, y, x+r, y+r, fill=gr.color_rgb(red, green, blue)))
+    ball.append(vx)
+    ball.append(vy)
+    ball.append(x)
+    ball.append(y)
 
 
 def tick_handler():
-    global ball
-    for j in range(5):
-        canvas.move(ball[3*j], ball[3*j+1], ball[3*j+2])
+    global ball, r
+    for j in range(20):
+        x_velocity = ball[5 * j+1]
+        y_velocity = ball[5 * j+2]
+        x_ball= ball[5 * j+3]
+        x_ball = ball[5 * j + 4]
+        if x_ball < 0:
+            x_velocity *= -1
+            x_ball = 0
+        elif x_ball > 600 - r:
+            x_velocity *= -1
+            x_ball = 600 - r
+        if x_ball < 0:
+            y_velocity *= -1
+            x_ball = 0
+        elif x_ball > 600 - r:
+            y_velocity *= -1
+            x_ball = 600 - r
+        x_ball += x_velocity
+        x_ball += y_velocity
+        canvas.move(ball[5*j], x_velocity, y_velocity)
+        ball[5*j + 1] = x_velocity
+        ball[5*j + 2] = y_velocity
+        ball[5*j + 3] = x_ball
+        ball[5*j + 4] = x_ball
 
 
 def time_handler():
@@ -31,13 +59,8 @@ def time_handler():
     root.after(sleep_dt, time_handler)
 
 
-speed_scale = Scale(root, orient=HORIZONTAL, length=300,
-               from_=0, to=10, tickinterval=1, resolution=1)
+speed_scale = Scale(root, orient=HORIZONTAL, length=300, from_=0, to=10, tickinterval=1, resolution=1)
 speed_scale.pack()
-
-# Скорость = 1
-speed_scale.set(1)
-freeze = False
 
 root.after(10, time_handler)
 root.mainloop()
