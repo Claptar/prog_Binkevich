2e51d3f0d447576c1fa707e0db2e96e7f99cd149
Claptar <aleksandr.binkevich@gmail.com>
2018-11-01 22:04:17 +0300

    Не хотел работать, то ли неправильно создавался лист в листе, то ли неправильно обращался к элементам листов. Сделал не очевидно, теперь данные о кругах записываются поочереди в один лист и вызываются с периодом 3.

--- ballgame/ballgame.py
+++ ballgame/ballgame.py
@@ -6,44 +6,30 @@ root.geometry("800x800")
 canvas = Canvas(root)
 r = 40
 canvas.pack(fill=BOTH, expand=1)
-balls = []
 ball = []
 
 for i in range(5):
     x = random.randint(100, 600)
     y = random.randint(100, 600)
-    Vx = random.randint(1, 5)*i
-    Vy = random.randint(1, 5)*i
-    ball.append(canvas.create_oval(x, y, x+r, y+r))
+    Vx = random.randint(1, 3)*i
+    Vy = random.randint(1, 3)*i
+    ball.append(canvas.create_oval(x, y, x+r, y+r, fill = "red"))
     ball.append(Vx)
     ball.append(Vy)
-    balls.append(ball)
 
 
 def tick_handler():
-    global balls, ball, x, y
+    global ball
     for j in range(5):
-        canvas.move(balls[j-1][0], balls[j-1][1], balls[j-1][2])
+        canvas.move(ball[3*j], ball[3*j+1], ball[3*j+2])
 
 
 def time_handler():
-    global freeze
     speed = speed_scale.get()
-    if speed == 0:
-        print("Заморозка!")
-        freeze = True
-        return
     tick_handler()
     sleep_dt = 1100 - 100*speed
     root.after(sleep_dt, time_handler)
 
-def unfreezer(event):
-    global freeze
-    if freeze == True:
-        speed = speed_scale.get()
-        if speed != 0:
-            freeze = False
-            root.after(0, time_handler)
 
 speed_scale = Scale(root, orient=HORIZONTAL, length=300,
                from_=0, to=10, tickinterval=1, resolution=1)
@@ -54,5 +40,4 @@ speed_scale.set(1)
 freeze = False
 
 root.after(10, time_handler)
-speed_scale.bind("<Motion>", unfreezer)
 root.mainloop()
