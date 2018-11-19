e510e7253cead46acec4614d24d96185c5b9dddc
Claptar <aleksandr.binkevich@gmail.com>
2018-11-19 18:16:03 +0300

    Заменио название переменной R и добавил комментарий.

--- ball/Catch_a_ball.py
+++ ball/Catch_a_ball.py
@@ -8,7 +8,7 @@ root = Tk()
 root.geometry('800x600')
 canv = Canvas(root, bg='white')
 Score = 0
-R = 20
+Radius = 20
 Miss = 0
 Clicks = 0
 Score_label = canv.create_text(80, 20, text='Score = {}'.format(Score), font='Arial 25')
@@ -16,23 +16,29 @@ Miss_label = canv.create_text(110, 60, text='missclicks = {} '.format(Miss), fon
 
 
 def balloon():
-    global circle_x, circle_y, R
+    global circle_x, circle_y, Radius
     circle_x = random.randint(20, 580)
     circle_y = random.randint(20, 580)
     red = random.randint(100, 255)
     green = random.randint(100, 255)
     blue = random.randint(100, 255)
-    canv.create_oval(circle_x-R, circle_y-R, circle_x+R, circle_y+R, fill=gr.color_rgb(red, green, blue))
+    canv.create_oval(circle_x-Radius, circle_y-Radius, circle_x+Radius, circle_y+Radius,
+                     fill=gr.color_rgb(red, green, blue))
 
 
 def left_click(event):
+    """
+    :x: x координата клика мышкой
+    :y: y координата клика мышкой 
+    :return: 
+    """
     global Clicks, Score_label, Miss_label
     x = event.x
     y = event.y
     global Score, Miss
-    if math.sqrt((circle_x - x)*(circle_x - x)+(circle_y - y)*(circle_y - y)) < R+1 and Clicks == 0:
+    if math.sqrt((circle_x - x)*(circle_x - x)+(circle_y - y)*(circle_y - y)) < Radius+1 and Clicks == 0:
         Score += 1
-    elif not math.sqrt((circle_x - x)*(circle_x - x)+(circle_y - y)*(circle_y - y)) < R+1:
+    elif not math.sqrt((circle_x - x)*(circle_x - x)+(circle_y - y)*(circle_y - y)) < Radius+1:
         Miss += 1
     Clicks += 1
     canv.delete(Score_label, Miss_label)
