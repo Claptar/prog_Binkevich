384102d47ede3d8a71dd6f11b8b0586ccf5e704e
Claptar <aleksandr.binkevich@gmail.com>
2018-11-20 01:33:24 +0300

    Подправил пеп8. Убрал scale, потому-что мне не нравится как это выглядит, поставил три шарика для увеличения сложности. Добавил комментарии для некоторых функций.

--- OOP/canon.py
+++ OOP/canon.py
@@ -6,11 +6,12 @@ root = Tk()
 fr = Frame(root)
 root.geometry('1000x800')
 canv = Canvas(root, bg='white')
-n = 10
+n = 3
 time_counter = 0
-g = 9.8  # Ускорение свободного падения для снаряда.
+G = 9.8  # Ускорение свободного падения для снаряда.
+Standard_Radius = 10
 Balls = []
-for i in range(n):
+for b in range(n):
     Balls.append(0)
 score = 0
 score_text = canv.create_text(200, 60, text='Попадания score = {} '.format(score), font='Arial 25', )
@@ -39,6 +40,27 @@ class Vector:
         self.x *= constant
         self.y *= constant
 
+    def vector_scal_multiplication(self, vector):
+        """
+        :param vector: векор, на который умножается исходный вектор
+        :return: результат скалярного умножения векторов
+        """
+        return self.x*vector.x + self.y*vector.y
+
+    def sum(self, vector):
+        """
+        :param vector: вектор, который складываетяс с данным
+        :return: результат сложения векторов
+        """
+        return Vector(self.x + vector.x, self.y + vector.y)
+
+    def unit_vector(self):
+        """
+        :return: единичный вектор от данного
+        """
+        return Vector(self.x / math.sqrt(self.x ** 2 + self.y ** 2),
+                      self.y / math.sqrt(self.x ** 2 + self.y ** 2))
+
 
 class Cannon:
     max_velocity = 100
@@ -49,7 +71,6 @@ class Cannon:
         self.y = y = 550
         self.shell_num = 1
         self.direction = math.pi/4
-        self.power = 0
         self.power_speed = 0
         self.cannon_diametr = 80
         self.line_length = 80
@@ -83,8 +104,8 @@ class Cannon:
         :return: экземпляр снаряда типа Shell
         """
         if len(shells) < 10:
-            self.time_length = self.stop_time - self.start_time
-            self.power_speed = (self.power * self.time_length)/3
+            time_length = self.stop_time - self.start_time
+            self.power_speed = time_length
             shell = Shell(self.x + 40 + self.line_length*math.cos(self.direction),
                           self.y + 40 + self.line_length*math.sin(self.direction),
                           self.power_speed, self.power_speed, self.canvas, self.direction)
@@ -114,18 +135,17 @@ class Cannon:
 
 
 class Shell:
-    global standard_radius
-    standard_radius = 10
+    global Standard_Radius
 
-    def __init__(self, x, y, Vx, Vy, canvas, direction):
+    def __init__(self, x, y, vx, vy, canvas, direction):
         self.x, self.y = x, y
-        self.Vx, self.Vy = Vx, Vy
+        self.vx, self.vy = vx, vy
         self.direction = direction
-        self.r = standard_radius
-        x1 = x - standard_radius
-        y1 = y - standard_radius
-        x2 = x + standard_radius
-        y2 = y + standard_radius
+        self.r = Standard_Radius
+        x1 = x - Standard_Radius
+        y1 = y - Standard_Radius
+        x2 = x + Standard_Radius
+        y2 = y + Standard_Radius
         self.delta_x = 0
         self.delta_y = 0
 
@@ -142,20 +162,20 @@ class Shell:
         :return: Движущийся снаряд
         """
 
-        ax, ay = 0, g
-        self.delta_x = self.Vx * dt * math.cos(self.direction) + ax * (dt ** 2) / 2
-        self.delta_y = self.Vy * dt * math.sin(self.direction) + ay * (dt ** 2) / 2
+        ax, ay = 0, G
+        self.delta_x = self.vx * dt * math.cos(self.direction) + ax * (dt ** 2) / 2
+        self.delta_y = self.vy * dt * math.sin(self.direction) + ay * (dt ** 2) / 2
         self.x += self.delta_x
         self.y += self.delta_y
-        self.Vx += ax * dt
-        self.Vy += -ay * dt
+        self.vx += ax * dt
+        self.vy += -ay * dt
 
         self.draw()
 
         if self.y > 600:
             self.canvas.delete(self.oval)
         if self.x > 1000:
-            self.Vx = -self.Vx
+            self.vx = -self.vx
 
     def draw(self):
         """
@@ -176,13 +196,13 @@ class Shell:
 
 
 class Target:
-    standard_radius = 50
+    Standard_Radius = 50
 
-    def __init__(self, x, y, Vx, Vy, red, green, blue, r):
+    def __init__(self, x, y, vx, vy, red, green, blue, r):
         self.x = x
         self.y = y
-        self.Vx = Vx
-        self.Vy = Vy
+        self.vx = vx
+        self.vy = vy
         self.design = canv.create_oval(x, y, x + r, y + r, fill=gr.color_rgb(red, green, blue))
         self.r = r
 
@@ -194,15 +214,20 @@ class Target:
         :return:
         """
         if (self.x + self.r > 1000) or (self.x - self.r < 300):
-            self.Vx *= -1
+            self.vx *= -1
         elif (self.y + self.r > 600) or (self.y - self.r < 0):
-            self.Vy *= -1
-        canv.move(self.design, self.Vx, self.Vy)
-        self.x += self.Vx
-        self.y += self.Vy
+            self.vy *= -1
+        canv.move(self.design, self.vx, self.vy)
+        self.x += self.vx
+        self.y += self.vy
 
 
 def hit_checker(shell, target):
+    """
+        :param shell: снаряд
+        :param target: шарик-мишень
+        :return: столкнулись или нет
+        """
     if shell == 0 or target == 0:
         return False
     if (shell.x - target.x) ** 2 + (shell.y - target.y) ** 2 <= (target.r + shell.r) ** 2:
@@ -214,33 +239,32 @@ def collide(ball_1, ball_2):
     Рассчёт столкновения двух шариков
     """
     if (ball_1.x - ball_2.x) ** 2 + (ball_1.y - ball_2.y) ** 2 <= ball_1.r ** 2:
-        v1 = Vector(ball_1.Vx, ball_1.Vy)
-        v2 = Vector(ball_2.Vx, ball_2.Vy)
+        v1 = Vector(ball_1.vx, ball_1.vy)
+        v2 = Vector(ball_2.vx, ball_2.vy)
         line = Vector(ball_1.x - ball_2.x, ball_1.y - ball_2.y)
-        line.x = line.x / math.sqrt((ball_1.x - ball_2.x) ** 2 + (ball_1.y - ball_2.y) ** 2)
-        line.y = line.y / math.sqrt((ball_1.x - ball_2.x) ** 2 + (ball_1.y - ball_2.y) ** 2)
-        normal = Vector(-line.y, line.x)
-        v_line_1 = v1.x * line.x + v1.y * line.y
-        v_line_2 = v2.x * line.x + v2.y * line.y
-        v_normal_1 = v1.x * normal.x + v1.y * normal.y
-        v_normal_2 = v2.x * normal.x + v2.y * normal.y
+        line = line.unit_vector()
+        normal = line.normal_vector()
+        v_line_1 = v1.vector_scal_multiplication(line)
+        v_line_2 = v2.vector_scal_multiplication(line)
+        v_normal_1 = v1.vector_scal_multiplication(normal)
+        v_normal_2 = v2.vector_scal_multiplication(normal)
         v_line_1, v_line_2 = v_line_2, v_line_1
-        ball_1.Vx = v_line_1 * line.x + v_normal_1 * normal.x
-        ball_1.Vy = v_line_1 * line.y + v_normal_1 * normal.y
-        ball_2.Vx = v_line_2 * line.x + v_normal_2 * normal.x
-        ball_2.Vy = v_line_2 * line.y + v_normal_2 * normal.y
+        ball_1.vx = v_line_1 * line.x + v_normal_1 * normal.x
+        ball_1.vy = v_line_1 * line.y + v_normal_1 * normal.y
+        ball_2.vx = v_line_2 * line.x + v_normal_2 * normal.x
+        ball_2.vy = v_line_2 * line.y + v_normal_2 * normal.y
 
 
 def target_creator():
         r = 50
         x = random.randint(400, 800)
         y = random.randint(100, 600)
-        Vx = random.randint(-3, 3)
-        Vy = random.randint(-3, 3)
+        vx = random.randint(-3, 3)
+        vy = random.randint(-3, 3)
         red = random.randint(50, 255)
         green = random.randint(50, 255)
         blue = random.randint(50, 255)
-        return Target(x, y, Vx, Vy, red, green, blue, r)
+        return Target(x, y, vx, vy, red, green, blue, r)
 
 
 def mouse_move_handler(event):
@@ -282,7 +306,6 @@ def tick():
                     canv.delete(score_text)
                     score_text = canv.create_text(200, 60, text='Попадания score = {} '.format(score), font='Arial 25')
 
-
     root.after(10, tick)
 
 
@@ -310,22 +333,11 @@ def time_stop(event):
     time_counter = 0
 
 
-def power_maker(event):
-        cannon.power = 2 * power_scale.get()
-
-
 canv.pack(fill=BOTH, expand=1)
-
-power_scale = Scale(root, orient=HORIZONTAL,
-                length=300,
-                from_=0, to=10, tickinterval=1, resolution=1, width=5)
-power_scale.pack()
-power_scale.set(1)
 
 canv.bind('<Motion>', mouse_move_handler)
 canv.bind("<ButtonPress-1>", time_start)
 canv.bind("<ButtonRelease-1>", time_stop)
-power_scale.bind("<Motion>", power_maker)
 shells = []
 cannon = Cannon(canv)
 
