16479d4ced696a5bf6bc2998ff89c4b2608d214e
Claptar <aleksandr.binkevich@gmail.com>
2018-11-22 23:32:57 +0300

    Добавил функции героя

--- dragons/hero.py
+++ dragons/hero.py
@@ -9,5 +9,23 @@ class Hero(Attacker):
         self._experience = 0
         self.name = name
 
+   def attack(self, target):
+        target._health -= self._attack
+        print(target._name, target._health, '/', target._healthfull, 'HP')
+
+    def exp_gain(self, target):
+        self._experience += target._experience
+
+    def is_lvl_up(self):
+        if self._experience > 99:
+            return True
+
+    def lvl_up(self):
+        self._attack += 10
+        self._health = 100
+
+    def exp_down(self):
+        self._experience = 0
+
 
 
