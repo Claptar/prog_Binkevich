b669a9c89963c890516b078f4a5fda58ed4ce0f2
Claptar <aleksandr.binkevich@gmail.com>
2018-12-02 22:29:22 +0300

    Глобальные переменные теперь выделены капсом. Поставил нестрогий знак при сравнении растояния от точки клика, до центра шарика. Теперь, если игрок не успел попасть в шарик в отведенное время, засчитывается промах. Поставил пробел в "root.bind('<Button-1>', (тут)left_click)", но пайчарм у меня не ругается на отсутствие пробела там.

--- ball/Catch_a_ball.py
+++ ball/Catch_a_ball.py
@@ -7,22 +7,22 @@ import math
 root = Tk()
 root.geometry('800x600')
 canv = Canvas(root, bg='white')
-Score = 0
-Radius = 20
-Miss = 0
-Clicks = 0
-Score_label = canv.create_text(80, 20, text='Счёт = {}'.format(Score), font='Arial 25')
-Miss_label = canv.create_text(110, 60, text=' Промахи = {} '.format(Miss), font='Arial 25')
+SCORE = 0
+RADIUS = 20
+MISS = 0
+CLICKS = 0
+Score_label = canv.create_text(80, 20, text='Счёт = {}'.format(SCORE), font='Arial 25')
+Miss_label = canv.create_text(110, 60, text=' Промахи = {} '.format(MISS), font='Arial 25')
 
 
 def balloon():
-    global circle_x, circle_y, Radius
+    global circle_x, circle_y, RADIUS
     circle_x = random.randint(20, 580)
     circle_y = random.randint(20, 580)
     red = random.randint(100, 255)
     green = random.randint(100, 255)
     blue = random.randint(100, 255)
-    canv.create_oval(circle_x-Radius, circle_y-Radius, circle_x+Radius, circle_y+Radius,
+    canv.create_oval(circle_x-RADIUS, circle_y-RADIUS, circle_x+RADIUS, circle_y+RADIUS,
                      fill=gr.color_rgb(red, green, blue))
 
 
@@ -32,30 +32,32 @@ def left_click(event):
     :y: y координата клика мышкой 
     :return: 
     """
-    global Clicks, Score_label, Miss_label
+    global CLICKS, Score_label, Miss_label
     x = event.x
     y = event.y
-    global Score, Miss
-    if math.sqrt((circle_x - x)**2+(circle_y - y)**2) < Radius+1 and Clicks == 0:
-        Score += 1
-    elif not math.sqrt((circle_x - x)**2+(circle_y - y)**2) < Radius+1:
-        Miss += 1
-    Clicks += 1
+    global SCORE, MISS
+    if math.sqrt((circle_x - x)**2+(circle_y - y)**2) <= RADIUS and CLICKS == 0:
+        SCORE += 1
+    elif not math.sqrt((circle_x - x)**2+(circle_y - y)**2) <= RADIUS:
+        MISS += 1
+    CLICKS += 1
     canv.delete(Score_label, Miss_label)
-    Score_label = canv.create_text(80, 20, text='Счёт = {}'.format(Score), font='Arial 25')
-    Miss_label = canv.create_text(110, 60, text=' Промахи = {} '.format(Miss), font='Arial 25')
+    Score_label = canv.create_text(80, 20, text='Счёт = {}'.format(SCORE), font='Arial 25')
+    Miss_label = canv.create_text(110, 60, text=' Промахи = {} '.format(MISS), font='Arial 25')
 
 
 def tick():
-    global Clicks, Score_label, Miss_label
-    Clicks = 0
+    global CLICKS, Score_label, Miss_label, MISS
+    if CLICKS == 0:
+        MISS += 1
+    CLICKS = 0
     root.after(1000, tick)
     canv.delete(ALL)
     balloon()
-    Score_label = canv.create_text(80, 20, text='Счёт = {}'.format(Score), font='Arial 25')
-    Miss_label = canv.create_text(110, 60, text=' Промахи = {}'.format(Miss), font='Arial 25')
+    Score_label = canv.create_text(80, 20, text='Счёт = {}'.format(SCORE), font='Arial 25')
+    Miss_label = canv.create_text(110, 60, text=' Промахи = {}'.format(MISS), font='Arial 25')
 
-root.bind('<Button-1>', left_click)
+root.bind('<Button-1>',  left_click)
 
 
 canv.pack(fill=BOTH, expand=1)
